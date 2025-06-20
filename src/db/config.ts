6a222ff12d6deaca9db1e776af085eca356955ce
Néstor Iriondo <nestor.iriondo@murrelektronik.de>
2025-06-20 08:19:44 +0200

    feat: add Report entity and update database configuration

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,8 +1,9 @@
 import { User } from 'src/users/user.entity';
+import { Report } from 'src/reports/report.entity';
 
 export const dbConfig = {
   type: 'sqlite' as const,
   database: 'db.sqlite',
   synchronize: true,
-  entities: [User],
+  entities: [User, Report],
 };
