e8677c92d24fc3a5f39b8c3aeb91b1d914ecb05d
Néstor Iriondo <nestor.iriondo@murrelektronik.de>
2025-06-20 08:44:45 +0200

    feat: update User entity to include name field and enhance database configuration comments

--- src/db/config.ts
+++ src/db/config.ts
@@ -4,6 +4,6 @@ import { Report } from 'src/reports/report.entity';
 export const dbConfig = {
   type: 'sqlite' as const,
   database: 'db.sqlite',
-  synchronize: true,
+  synchronize: true, // Means that the database schema will be automatically created on every application launch
   entities: [User, Report],
 };
