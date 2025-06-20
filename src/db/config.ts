134bcabe47f9e3c286451faf2bfd4fa4e8cd3c4e
Néstor Iriondo <nestor.iriondo@murrelektronik.de>
2025-06-20 08:14:58 +0200

    feat: add user entity and configure database with user entity

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,6 +1,8 @@
+import { User } from 'src/users/user.entity';
+
 export const dbConfig = {
   type: 'sqlite' as const,
   database: 'db.sqlite',
   synchronize: true,
-  entities: [],
+  entities: [User],
 };
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   providers: [UsersService],
+  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
